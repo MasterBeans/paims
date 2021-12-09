f520ffcf83fdefa7c4a745fa74386fca722489fd
MasterBeans <jul21002@byui.edu>
2021-12-09 23:07:12 +0800

    moved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'P-AIMS Assistant')
 
 @section('css')
 <link rel="stylesheet" href="/css/app.css">
@@ -84,7 +84,7 @@
 
 
     <div class="row">
-      <div class="col-6">
+      <div class="col-12">
         <div class="card">
           <div class="card-header">
             <h3 class="card-title">Employer Engagement</h3>
